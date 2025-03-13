96f228433732629d8fd1b9392ebccfa1b9b83b92
shatru <shatrudhansingh7388@gmail.com>
2025-03-14 03:19:31 +0530

    all commit

--- DemoPartialView/Controllers/EmployeeController.cs
+++ DemoPartialView/Controllers/EmployeeController.cs
@@ -15,7 +15,8 @@ namespace DemoPartialView.Controllers
             Employee emp1 = new Employee { Id = 1001, Name = "Rani", Job = "maneger", Salary = 50000.00, City = "Delhi", Photo = "/Images/lahga1.png", Status = true };
             Employee emp2 = new Employee { Id = 1002, Name = "Arti", Job = "HR", Salary = 60000.00, City = "Delhi", Photo = "/Images/lahga2j.jpg", Status = true };
             Employee emp3= new Employee { Id = 1003, Name = "Pooja", Job = "Developer", Salary = 70000.00, City = "Mumbai", Photo ="Images/lahga4p.png", Status = true };
-            List<Employee> list = new List<Employee> { emp1,emp2,emp3};
+            Employee emp4 = emp3;
+            List<Employee> list = new List<Employee> { emp1,emp2,emp3, emp4 };
             return View(list);
         }
     }
